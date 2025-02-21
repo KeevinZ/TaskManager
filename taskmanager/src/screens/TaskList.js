961e6df2515333ac7ffd4175ec3bd4d829353084
keevinz <kevin1080p.mdm@gmail.com>
2025-02-20 22:32:20 -0300

    Criação do cabeçalho da Função TaksList

--- taskmanager/src/screens/TaskList.js
+++ taskmanager/src/screens/TaskList.js
@@ -1,17 +1,56 @@
-import {Text, View, StyleSheet} from 'react-native'
+import { Text, View, StyleSheet, ImageBackground } from 'react-native'
+import  todayImage from '../../assets/imgs/today.jpg'
+import moment from "moment"
+import 'moment/locale/pt-br'
 
-export default function TaskList(){
-    return(
+export default function TaskList() {
+    const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+
+    return (
         <View style={styles.container}>
-            <Text>TaskList</Text>
+
+            <ImageBackground source={todayImage} style={styles.background}>
+                <View style={styles.titlleBar}>
+                <Text style={styles.titlle}>Hoje</Text>        
+                <Text style={styles.subtitlle}>{today}</Text>
+                </View>
+            </ImageBackground>
+
+            <View style={styles.taskList}>
+
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+        
+    },
+    background: {
+        flex: 3,
+    },
+    taskList: {
+        flex: 7
+    },
+    titlleBar: {
+     flex: 1,
+     justifyContent: 'flex-end',   
+  
+    },
+    titlle: {
+        color: 'white',
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20
+  
+    },
+    subtitlle: {
+        color: 'white',
+        fontSize: 20,
+        marginLeft: 10,
+        marginBottom: 20
+    },
 })
